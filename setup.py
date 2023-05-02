191cf6d75b7e2485ea502c18d765789e2655fad7
Sierra Brown <sierra@millionconcepts.com>
2023-05-02 15:20:26 -0400

    add url to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name="pretty_plot", version="0.2.0", packages=find_packages())
+setup(name="pretty_plot",
+      version="0.2.0",
+      url="https://github.com/millionconcepts.pretty-plot.git",
+      packages=find_packages())

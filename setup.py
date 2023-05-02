9485fa3de3bcb4657cded513eba7a21be4fa1c80
Sierra Brown <sierra@millionconcepts.com>
2023-05-02 15:03:53 -0400

    changing to underscore

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name="pretty-plot", version="0.2.0", packages=find_packages())
+setup(name="pretty_plot", version="0.2.0", packages=find_packages())

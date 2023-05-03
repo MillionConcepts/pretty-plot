59ee280357937c093d6b9393aad87bc73e4bd0da
michael <mstclair@millionconcepts.com>
2023-05-03 15:05:00 -0400

    reorganize setup

--- setup.py
+++ setup.py
@@ -5,7 +5,5 @@ setup(
       version="0.2.0",
       url="https://github.com/millionconcepts.pretty-plot.git",
       packages=find_packages(),
-      package_data={
-            "pretty_plot": ['static/*.*', 'data/*.*']
-      }
+      include_package_data=True,
 )
